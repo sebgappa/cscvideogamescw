4f7cf9ecfd9b93128e3c2ec4a46c21b205f758db
UKGC <seb.gappa@gmail.com>
2021-03-11 17:15:59 +0000

    Upgraded build setting to x86_64, created prefabs for all things, saved scene, started working on movement script

---
+++ Assets/Scripts/Behaviours/Player/StandardMovementController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class GenericMovementControl : MonoBehaviour
+{
+    [SerializeField]
+    private float speed;
+    private Vector2 velocity;
+    private Rigidbody2D rigidBody2D;
+
+    private void Awake()
+    {
+        rigidBody2D = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Move(float xDirection, float yDirection)
+    {
+        velocity = rigidBody2D.velocity;
+        velocity.x = xDirection * speed; 
+        velocity.x = yDirection * speed; 
+    }
+
+
+}
