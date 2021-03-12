8fe2a6ccd2cea1e964b1f435b4aeb00d79f55b6d
UKGC <seb.gappa@gmail.com>
2021-03-12 14:39:52 +0000

    Adding walking and idle animations!

--- Assets/Scripts/Behaviours/Player/PlayerMovement.cs
+++ Assets/Scripts/Behaviours/Player/PlayerMovement.cs
@@ -5,21 +5,34 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
     public float speed;
-    private Vector2 newVelocity;
+    private Animator animator;
     private Rigidbody2D rigidBody2D;
+    private SpriteRenderer spriteRenderer;
 
     public void OnMove(InputAction.CallbackContext context)
     {
         var direction = context.action.ReadValue<Vector2>();
 
-        newVelocity = rigidBody2D.velocity;
+        if (direction.x < 0)
+        {
+            spriteRenderer.flipX = false;
+        } else if(direction.x > 0)
+        {
+            spriteRenderer.flipX = true;
+        }
+
+        var newVelocity = rigidBody2D.velocity;
         newVelocity.x = direction.x * speed;
         newVelocity.y = direction.y * speed;
         rigidBody2D.velocity = newVelocity;
+
+        animator.SetFloat("Speed", Mathf.Abs(newVelocity.x + newVelocity.y));
     }
 
     private void Awake()
     {
         rigidBody2D = gameObject.GetComponent<Rigidbody2D>();
+        spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+        animator = gameObject.GetComponent<Animator>();
     }
 }
