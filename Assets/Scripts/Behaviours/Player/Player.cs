f16c91cee12d5d1ccd68df2003b47069bbf616e3
UKGC <seb.gappa@gmail.com>
2021-04-26 00:00:23 +0100

    UI Score

--- Assets/Scripts/Behaviours/Player/Player.cs
+++ Assets/Scripts/Behaviours/Player/Player.cs
@@ -11,6 +11,7 @@ public class Player : MonoBehaviour
     private PlayerInput playerInput;
     private Rigidbody2D rigidBody2D;
     private BoxCollider2D boxCollider2D;
+    private Score score;
 
     public void TakeDamage(int damage)
     {
@@ -29,8 +30,19 @@ public class Player : MonoBehaviour
         animator.SetBool("isDead", true);
         boxCollider2D.enabled = false;
         playerInput.enabled = false;
+        rigidBody2D.velocity = new Vector2(0, 0);
+        score.IncreaseScore();
         yield return new WaitForSeconds(3);
-        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        Revive();
+    }
+
+    private void Revive()
+    {
+        animator.SetBool("isDead", false);
+        animator.SetTrigger("Revive");
+        currentHealth = maxHealth;
+        boxCollider2D.enabled = true;
+        playerInput.enabled = true;
     }
 
     // Start is called before the first frame update
@@ -45,5 +57,6 @@ public class Player : MonoBehaviour
         playerInput = gameObject.GetComponent<PlayerInput>();
         rigidBody2D = gameObject.GetComponent<Rigidbody2D>();
         boxCollider2D = gameObject.GetComponent<BoxCollider2D>();
+        score = gameObject.GetComponent<Score>();
     }
 }
---
+++ Assets/Scripts/Behaviours/Score/Score.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+    public Text scoreText;
+    private int score = 0;
+
+    public void IncreaseScore()
+    {
+        score++;
+        scoreText.text = score.ToString();
+    }
+}
