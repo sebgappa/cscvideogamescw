d371efb828a647d802357d621feb92507f5fd316
UKGC <seb.gappa@gmail.com>
2021-04-26 22:04:06 +0100

    added game menu

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    private int gameModeToLoad = 1;
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(gameModeToLoad);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit");
+        Application.Quit();
+    }
+
+    public void ChangeGamemode(int val)
+    {
+        gameModeToLoad = val + 1;
+    }
+}
