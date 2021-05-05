701f92e0fe22ff01bf3b0f2eab881e3bc0e4c4a2
UKGC <seb.gappa@gmail.com>
2021-05-05 22:11:27 +0100

    pause screen

---
+++ Assets/Scripts/Behaviours/UI/PauseScreen.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+
+public class PauseScreen : MonoBehaviour
+{
+    public static bool Paused = false;
+    public GameObject pauseMenu;
+
+    public void OnPause(InputAction.CallbackContext input)
+    {
+        if (!input.started) return;
+
+        if (Paused)
+        {
+            Resume();
+        }
+        else
+        {
+            Pause();
+        }
+    }
+    public void Resume()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+        Paused = false;
+    }
+
+    private void Pause()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+        Paused = true;
+    }
+
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene(0);
+        Resume();
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
