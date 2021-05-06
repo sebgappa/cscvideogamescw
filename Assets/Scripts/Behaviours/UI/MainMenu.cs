b79798641881196cffed671952debc56b9b61cde
UKGC <seb.gappa@gmail.com>
2021-05-06 23:31:14 +0100

    updating menu for tutorial

--- Assets/Scripts/Behaviours/UI/MainMenu.cs
+++ Assets/Scripts/Behaviours/UI/MainMenu.cs
@@ -4,6 +4,7 @@ using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
     private int gameModeToLoad = 1;
+    private int tutorialIndex = 3;
     public void PlayGame()
     {
         SceneManager.LoadScene(gameModeToLoad);
@@ -17,5 +18,10 @@ public class MainMenu : MonoBehaviour
     public void ChangeGamemode(int val)
     {
         gameModeToLoad = val + 1;
+    }
+
+    public void LoadTutorial()
+    {
+        SceneManager.LoadScene(tutorialIndex);
     }
 }
